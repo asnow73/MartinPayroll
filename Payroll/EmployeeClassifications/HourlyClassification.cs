783b13d2f448ef1b2306073791459265ac318fd7
asnow73 <albertulgu@list.ru>
2014-01-21 21:36:28 +0300

    Add timecard function is completed

--- Payroll/EmployeeClassifications/HourlyClassification.cs
+++ Payroll/EmployeeClassifications/HourlyClassification.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,28 @@ namespace Payroll
 {
     public class HourlyClassification : PaymentClassification
     {
+        private Hashtable timeCards;
         private double hourlyRate;
 
         public HourlyClassification(double hourlyRate)
         {
             this.hourlyRate = hourlyRate;
+            this.timeCards = new Hashtable();
         }
 
         public double HourlyRate
         {
             get { return hourlyRate; }
+        }
+
+        public void AddTimeCard(TimeCard tc)
+        {
+            timeCards[tc.Date] = tc;
+        }
+
+        public TimeCard GetTimeCard(DateTime date)
+        {
+            return (timeCards[date] as TimeCard);
         }
     }
 }
---
+++ Payroll/Transactions/TimeCardTransaction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll
+{
+    public class TimeCardTransaction : Transaction
+    {
+        private DateTime date;
+        private double hours;
+        private int empId;
+
+        public TimeCardTransaction(DateTime dt, double hours, int id)
+        {
+            this.date = dt;
+            this.hours = hours;
+            this.empId = id;
+        }
+
+        public void Execute()
+        {
+            Employee e = PayrollDatabase.GetEmployee(empId);
+            if (e != null)
+            {
+                HourlyClassification hc = e.Classification as HourlyClassification;
+                if (hc != null)
+                    hc.AddTimeCard(new TimeCard(date, hours));
+                else
+                    throw new InvalidOperationException("Попытка добавить карточку учёта для работника не на почасовой оплате");
+            }
+            else
+            {
+                throw new InvalidOperationException("Работник не найден");
+            }
+        }
+    }
+}
